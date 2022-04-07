4cfc8d94b877500a6c202225bd5e8bb1db71e3f8
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2022-04-07 14:52:27 +0900

    feat: 공유를 위한 파일 업로드

---
+++ ios-MC1-DDD-project/ios-MC1-DDD-project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-MC1-DDD-project
+//
+//  Created by 이성노 on 2022/04/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-MC1-DDD-project/ios-MC1-DDD-project/ios_MC1_DDD_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_MC1_DDD_projectApp.swift
+//  ios-MC1-DDD-project
+//
+//  Created by 이성노 on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct ios_MC1_DDD_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
