c3540290154c4a642323ad2659223f7394c01ed0
hminkim <hminkim2@gmail.com>
2022-04-11 23:59:13 +0900

    4페이지, 5페이지 작업

---
+++ ios-MC1-DDD-project/ios-MC1-DDD-project/NextView.swift
@@ -0,0 +1,20 @@
+//
+//  NextView.swift
+//  ios-MC1-DDD-project
+//
+//  Created by kimhyeongmin on 2022/04/11.
+//
+
+import SwiftUI
+
+struct NextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NextView_Previews: PreviewProvider {
+    static var previews: some View {
+        NextView()
+    }
+}
