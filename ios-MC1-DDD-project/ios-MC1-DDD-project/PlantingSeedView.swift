77d56b262d72b8895ede7e7bd403867125502989
hminkim <hminkim2@gmail.com>
2022-04-13 03:26:52 +0900

    표정, 모양, 색깔 별 씨앗 심는 애니메이션 수정

--- ios-MC1-DDD-project/ios-MC1-DDD-project/PlantingSeedView.swift
+++ ios-MC1-DDD-project/ios-MC1-DDD-project/PlantingSeedView.swift
@@ -10,18 +10,20 @@ import SwiftUI
 struct PlantingSeedView: View {
     
     @State var viewTranseform = false
-    @State private var moving = -80
+    @State private var moving = -100
+    
+    @State var firstIsTappedNumber: Int = 6
+    @State var secondIsTappedNumber: Int = 6
+    @State var currentSeedColor: Int = 6
+    
+    @State var firstIsSelected = false
     
     var body: some View {
         
         NavigationView {
             ZStack {
                 
-                Image("PlantingSeed")
-                    .resizable()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .edgesIgnoringSafeArea(.all)
-                    .scaledToFill()
+                Color("BackgroundColor").edgesIgnoringSafeArea(.all)
                 
                 VStack(alignment: .leading) {
                     VStack(alignment: .leading) {
@@ -36,17 +38,30 @@ struct PlantingSeedView: View {
                 VStack {
                     Spacer()
                     
-                    Image("seed_example")
-                        .offset(y: CGFloat(moving))
-                        .onAppear(){
-                            withAnimation(.spring(response:  1, dampingFraction: 0.7, blendDuration: 0).delay(0.5)){
-                                moving = 60
-                            }
+                    ZStack{
+                        Image("ShapePNG\(ChooseSecondDetailInformation(secondIsTappedNumber: $secondIsTappedNumber).secondIsTappedNumber)")
+                            .renderingMode(.template)
+                            .foregroundColor(Color("Color\(currentSeedColor)"))
+                            .scaleEffect(1.8)
+                        
+                        Image("ImojiPNG\(ChooseFirstDetailInformation(firstIsTappedNumber: $firstIsTappedNumber, firstIsSelected: $firstIsSelected).firstIsTappedNumber)")
+                            .renderingMode(.template)
+                            .foregroundColor(secondIsTappedNumber == 9 ? Color.white : Color("darkcolor"))
+                            
+                    }
+                    .offset(y: CGFloat(moving))
+                    .onAppear(){
+                        withAnimation(.spring(response:1, dampingFraction:0.8, blendDuration:0).delay(0.5)){
+                            moving = 220
                         }
-                        .scaleEffect(2)
+                    }
+                    .scaleEffect(1)
                     
                     Spacer()
                     
+                    Image("GroundForSowing")
+                        .renderingMode(.template)
+                        .foregroundColor(Color("Color\(currentSeedColor)"))
                 }
             }
         }
@@ -112,7 +127,7 @@ struct PlantingSeedView: View {
                     .onAppear{
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {}
                     }
-                }
+            }
             
             // Reference from :
             // https://github.com/chitomo12/SwiftUI-Text-Animation-Library?ref=iosexample.com#blur
