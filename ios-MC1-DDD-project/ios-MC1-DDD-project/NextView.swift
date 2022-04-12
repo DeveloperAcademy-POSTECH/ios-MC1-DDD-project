3d3845bba7345cac92ea6075fa9a44f4838a94b0
hminkim <hminkim2@gmail.com>
2022-04-12 13:34:05 +0900

    씨앗 모션 기능 생성

--- ios-MC1-DDD-project/ios-MC1-DDD-project/NextView.swift
+++ ios-MC1-DDD-project/ios-MC1-DDD-project/NextView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct NextView: View {
+    
+    @State private var moving = -100
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Text("A ball falling with bounce")
+                .font(.title)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+                Image("seed_example")
+                    .offset(y: CGFloat(moving))
+                    .onAppear(){
+                        withAnimation(.spring(response:  0.5, dampingFraction: 0.5, blendDuration: 0).delay(1).repeatForever(autoreverses: false)){
+                            moving = 10
+                        }
+            }
+            .scaleEffect(2)
+            
+            Spacer()
+            
+        }
     }
 }
 
 struct NextView_Previews: PreviewProvider {
     static var previews: some View {
         NextView()
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        
     }
 }
